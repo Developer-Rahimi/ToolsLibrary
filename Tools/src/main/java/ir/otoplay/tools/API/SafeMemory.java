a6102f5d2dd9144f672daba82f4fd92c9a636cf8
Developer <moosarahimi8@gmail.com>
2021-08-05 15:58:50 +0430

    add encrypt before send

--- Tools/src/main/java/ir/otoplay/tools/API/SafeMemory.java
+++ Tools/src/main/java/ir/otoplay/tools/API/SafeMemory.java
@@ -4,6 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import ir.otoplay.tools.Intefaces.OnSafeMemoryListener;
 import ir.otoplay.tools.Utils.SendMessage;
@@ -14,6 +19,7 @@ public class SafeMemory {
     private SafeMemoryReceiver safeMemoryReceiver=new SafeMemoryReceiver();
     private OnSafeMemoryListener safeMemoryListener;
     SendMessage sendMessage;
+    private  String K="12345678903216549870987654321012";
     public SafeMemory(Context context) {
         this.context = context;
         sendMessage=new SendMessage(context);
@@ -25,7 +31,11 @@ public class SafeMemory {
         sendMessage.SendSafeMemory("Read",Key,"");
     }
     public void WriteKey(String Key,String Data){
-        sendMessage.SendSafeMemory("Write",Key,Data);
+        try {
+            sendMessage.SendSafeMemory("Write",Key,encrypt(Data.getBytes(),GetKey()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public class SafeMemoryReceiver extends BroadcastReceiver {
 
@@ -36,7 +46,7 @@ public class SafeMemory {
                 String Data=intent.getStringExtra("Data");
                 Boolean Error=intent.getBooleanExtra("Error",false);
                 if(safeMemoryListener!=null){
-                    safeMemoryListener.Received(Target,Data,Error);
+                    safeMemoryListener.Received(Target,decrypt(Data.getBytes(),GetKey()),Error);
                 }
             }
             catch (Exception e){
@@ -46,5 +56,32 @@ public class SafeMemory {
     }
     public void setOnSafeMemoryListener(OnSafeMemoryListener safeMemoryListener){
         this.safeMemoryListener=safeMemoryListener;
+    }
+    private   String encrypt(byte[] plaintext, String key) throws Exception {
+        byte[] IV = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        Cipher cipher = Cipher.getInstance("AES");
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(IV);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        byte[] cipherText = cipher.doFinal(plaintext);
+        return new String(Base64.encode(cipherText,Base64.DEFAULT));
+    }
+    private   String decrypt(byte[] cipherText, String key) {
+        try {
+            byte[] IV = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            Cipher cipher = Cipher.getInstance("AES");
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivSpec = new IvParameterSpec(IV);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+            byte[] decryptedText = cipher.doFinal(cipherText);
+            return new String(Base64.decode(new String(decryptedText),Base64.DEFAULT));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private String GetKey(){
+        String pak=context.getPackageName();
+        return (pak+K).substring(0,32);
     }
 }
