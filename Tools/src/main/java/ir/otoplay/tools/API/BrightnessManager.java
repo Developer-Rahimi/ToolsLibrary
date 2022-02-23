905c29eeb64dcd8874885a17bbe0cf82eb40aa16
Developer <moosarahimi8@gmail.com>
2022-02-23 18:13:19 +0330

    add api set Default brightness

--- Tools/src/main/java/ir/otoplay/tools/API/BrightnessManager.java
+++ Tools/src/main/java/ir/otoplay/tools/API/BrightnessManager.java
@@ -2,13 +2,17 @@ package ir.otoplay.tools.API;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.provider.Settings;
+
+import ir.otoplay.tools.Utils.SendMessage;
 
 public class BrightnessManager {
     Context context;
-
+    private SendMessage sendMessage;
     public BrightnessManager(Context context) {
         this.context = context;
+        sendMessage=new SendMessage(context);
     }
     public void SetMode(int mode){
         if(mode<0)mode=2;
@@ -22,5 +26,8 @@ public class BrightnessManager {
     public void SetValue(int value){
         ContentResolver cResolver = context.getContentResolver();
         Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, value);
+    }
+    public void SetDefaultBRIGHTNESS(int value){
+        sendMessage.SendDeviceInformation("LCD","BRIGHTNESS",value+"");
     }
 }
